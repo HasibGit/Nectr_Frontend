d8f8454d7c75c7295b541141bc2cf181cb3d95ca
u1176456 <mdhasib.ullah@iqvia.com>
2025-04-21 00:57:09 +0600

    added guard to all protected routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,14 +7,19 @@ import { MessagesComponent } from './components/messages/messages.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent }, // Public route
+
+  // Protected routes
   {
-    path: 'members',
-    component: MembersListComponent,
+    path: '',
     canActivate: [authGuard],
+    children: [
+      { path: 'members', component: MembersListComponent },
+      { path: 'members/:id', component: MembersDetailComponent },
+      { path: 'lists', component: ListsComponent },
+      { path: 'messages', component: MessagesComponent },
+    ],
   },
-  { path: 'members/:id', component: MembersDetailComponent },
-  { path: 'lists', component: ListsComponent },
-  { path: 'messages', component: MessagesComponent },
-  { path: '**', component: HomeComponent, pathMatch: 'full' },
+
+  { path: '**', component: HomeComponent, pathMatch: 'full' }, // Wildcard route
 ];
