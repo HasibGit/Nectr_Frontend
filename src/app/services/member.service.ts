69d56939b91edc50c3453688e51fa6830a6572a0
u1176456 <mdhasib.ullah@iqvia.com>
2025-05-22 01:03:33 +0600

    added jwt interceptor

--- src/app/services/member.service.ts
+++ src/app/services/member.service.ts
@@ -1,9 +1,8 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { IMember } from '../interfaces/member.interface';
 import { Observable } from 'rxjs';
-import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,27 +10,12 @@ import { AuthService } from './auth.service';
 export class MemberService {
   baseUrl = environment.baseUrl;
   http = inject(HttpClient);
-  authService = inject(AuthService);
 
   getMembers(): Observable<IMember[]> {
-    return this.http.get<IMember[]>(
-      this.baseUrl + '/api/users',
-      this.getHttpOptions()
-    );
+    return this.http.get<IMember[]>(this.baseUrl + '/api/users');
   }
 
   getMember(username: string): Observable<IMember> {
-    return this.http.get<IMember>(
-      this.baseUrl + '/api/user/' + username,
-      this.getHttpOptions()
-    );
-  }
-
-  getHttpOptions() {
-    return {
-      headers: new HttpHeaders({
-        Authorization: `Bearer ${this.authService.loggedInUser()?.token}`,
-      }),
-    };
+    return this.http.get<IMember>(this.baseUrl + '/api/user/' + username);
   }
 }
