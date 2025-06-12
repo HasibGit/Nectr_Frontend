1714adf1cebd9ec9c1f7b1fd2026e8ab3daf2356
u1176456 <mdhasib.ullah@iqvia.com>
2025-06-12 20:36:01 +0600

    expanded upon register form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit, output } from '@angular/core';
 import {
+  FormBuilder,
   FormControl,
   FormGroup,
   FormsModule,
@@ -29,21 +30,30 @@ export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
 
   private authService = inject(AuthService);
+  private fb = inject(FormBuilder);
 
   ngOnInit(): void {
     this.initializeRegisterForm();
   }
 
   initializeRegisterForm(): void {
-    this.registerForm = new FormGroup(
+    this.registerForm = this.fb.group(
       {
-        username: new FormControl('', Validators.required),
-        password: new FormControl('', [
-          Validators.required,
-          Validators.minLength(8),
-          Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/),
-        ]),
-        confirmPassword: new FormControl('', Validators.required),
+        gender: ['male'],
+        username: ['', Validators.required],
+        knownAs: ['', Validators.required],
+        dateOfBirth: ['', Validators.required],
+        city: ['', Validators.required],
+        country: ['', Validators.required],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(8),
+            Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/),
+          ],
+        ],
+        confirmPassword: ['', Validators.required],
       },
       { validators: passwordMatchValidator('password', 'confirmPassword') }
     );
